99b5940b8038a2ac02f8aee1058a68e91d52f6c0
rajstudiosolapur <rajstudiosolapur@gmail.com>
2026-09-09 15:56:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -335,7 +335,7 @@ members = [
         "name": "Atul Swami",
         "role": "Founder & Managing Director of Raj Studio Solapur",
         "phone": "7507552822",
-        "photo": "artist1.jpg",
+        "photo": "artist1.png",
     },
     {
         "name": "Dhanashri Sargule",
